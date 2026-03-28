c34d4435ce1f5bfbfae62ff884edf0ec3ec29ab1
sofiane-lang <sofiane@businessentrepreneur.fr>
2026-03-28 04:01:52 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        mauve: '#460B5E',
+        mauveL: '#5E1280',
+        cream: '#F9F7F2',
+        orange: '#FF6400',
+        orangeH: '#E55900',
+        charcoal: '#1A1A1A',
+      },
+    },
+  },
   plugins: [],
 }
